78157514ce3177b46b2dcb11f5dbab72152a0fb9
AfrinaldiLibrary <afrinaldi190401@gmail.com>
2024-03-19 14:54:37 +0700

    ADD : on click marker show address

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:my_story_app/data/model/stories/stories_response.dart';
 import 'package:my_story_app/theme/color_schemes.dart';
 import 'package:my_story_app/theme/text_style.dart';
 
-class MyDetailScreen extends StatelessWidget {
+class MyDetailScreen extends StatefulWidget {
   final ListStory listStory;
 
   const MyDetailScreen({super.key, required this.listStory});
+
+  @override
+  State<MyDetailScreen> createState() => _MyDetailScreenState();
+}
+
+class _MyDetailScreenState extends State<MyDetailScreen> {
+  late GoogleMapController mapController;
+  final Set<Marker> markers = {};
+  late LatLng userLocation;
+  late List<Placemark> placemarks;
+
+  @override
+  void initState() {
+    super.initState();
+    userLocation = LatLng(widget.listStory.lat!, widget.listStory.lon!);
+    initializePlacemarks();
+  }
+
+  Future<void> initializePlacemarks() async {
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+        widget.listStory.lat!, widget.listStory.lon!);
+
+    if (placemarks.isNotEmpty) {
+      var placemark = placemarks.first;
+      var address = "${placemark.street}, ${placemark.locality}, ${placemark.postalCode}";
+
+      final marker = Marker(
+        markerId: MarkerId(widget.listStory.name),
+        position: userLocation,
+        infoWindow: InfoWindow(title: placemark.name, snippet: address),
+        onTap: () async {
+          mapController.animateCamera(
+            CameraUpdate.newLatLngZoom(userLocation, 18),
+          );
+        },
+      );
+
+      setState(() {
+        markers.add(marker);
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    mapController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +69,13 @@ class MyDetailScreen extends StatelessWidget {
               children: [
                 FadeInImage.assetNetwork(
                   placeholder: 'assets/images/placeholder.png',
-                  image: listStory.photoUrl,
+                  image: widget.listStory.photoUrl,
                   fit: BoxFit.fitWidth,
                   width: double.infinity,
                   height: 300,
                   imageErrorBuilder: (context, error, stackTrace) {
-                    return Image.asset('assets/images/image_not_available.jpg', fit: BoxFit.cover);
+                    return Image.asset('assets/images/image_not_available.jpg',
+                        fit: BoxFit.cover);
                   },
                 ),
                 const SafeArea(
@@ -35,7 +86,7 @@ class MyDetailScreen extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Text(
-                listStory.name,
+                widget.listStory.name,
                 style: myTextTheme.titleMedium?.copyWith(
                   color: Colors.black87,
                   fontWeight: FontWeight.bold,
@@ -45,10 +96,32 @@ class MyDetailScreen extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Text(
-                listStory.description,
+                widget.listStory.description,
                 style: myTextTheme.labelSmall?.copyWith(color: Colors.black45),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 300,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16.0),
+                  child: GoogleMap(
+                    initialCameraPosition: CameraPosition(
+                      zoom: 18,
+                      target: userLocation,
+                    ),
+                    markers: markers,
+                    onMapCreated: (controller) {
+                      setState(() {
+                        mapController = controller;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
